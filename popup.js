92c1c7f1b73e7af25b31d4119d04eb19cbd54c8a
Sineway <sineway@yandex.com>
2021-06-18 22:17:29 +0300

    Add starter popup template

--- popup.js
+++ popup.js
@@ -1,15 +1,34 @@
-import {PopupRenderer} from "/javascripts/renderers/popup.js";
-import {PresentableItem} from "/javascripts/models/presentable-item.js";
+import TimeUnit from "/api/TimeUnit.js";
 
-const renderer = new PopupRenderer();
-renderer.renderProgress();
+let $ = (selector, root = document) => {
+    return root.querySelector(selector);
+};
 
-chrome.runtime.getBackgroundPage(({dataLoaded}) => {
-    chrome.tabs.query({active: true, currentWindow: true}, ([tab]) => {
-        dataLoaded[tab.id].then((data) => {
-            renderer.renderSuccess(new PresentableItem(data));
-        }).catch((error) => {
-            renderer.renderFailure(error);
-        });
-    });
-});
+let $$ = (selector, root = document) => {
+    return root.querySelectorAll(selector);
+};
+
+// ...get estimates...
+
+$("body").replaceChildren($("#main-template").content);
+
+// ...iterate over estimates...
+
+    let fragment = $("#row-template").content.cloneNode(true);
+    // ...fill cells...
+    $("#row-template").before(fragment);
+
+    $("#rowgroup-row-template").before(...[
+        ["Hourly",  TimeUnit.HOUR ],
+        ["Daily",   TimeUnit.DAY  ],
+        ["Weekly",  TimeUnit.WEEK ],
+        ["Monthly", TimeUnit.MONTH],
+        ["Annualy", TimeUnit.YEAR ]
+    ].map(([title, value]) => {
+        let fragment = $("#rowgroup-row-template").content.cloneNode(true);
+        // ...fill cells...
+        let [c1, c2, c3, c4] = $$(".table__cell", fragment);
+        c1.textContent = title;
+        return fragment;
+    }));
+
