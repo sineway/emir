794f28ae07319d23544d81cb01223b25674e3745
Sineway <sineway@yandex.com>
2021-06-18 09:59:39 +0300

    Refactor background script

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
-import {Catalog} from "./gateways/catalog.js";
+import { Catalog } from "/javascripts/gateways/catalog.js";
 
-const pattern = new RegExp([
+let catalog = new Catalog;
+let pattern = new RegExp([
     "^https:\\/\\/",
     "(?:themeforest|codecanyon|videohive|audiojungle|graphicriver|photodune|3docean)\\.net",
     "\\/item",
@@ -8,30 +9,28 @@ const pattern = new RegExp([
     "\\/([0-9]+)"
 ].join(""));
 
-const loadData = (url) => {
-    const [, itemId] = pattern.exec(url);
-    return new Promise((resolve) => {
-        chrome.storage.sync.get(["apiToken"], resolve);
-    }).then(({apiToken}) => {
-        return new Catalog(apiToken).getItem(itemId);
-    });
+let loadData = async url => {
+    let [, itemId] = pattern.exec(url);
+    let auth = await browser.storage.sync.get(["apiToken"]);
+    return Object.assign(catalog, auth).getItem(itemId);
 };
 
 window.dataLoaded = {};
 
-chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
+browser.tabs.onUpdated.addListener((tabId, info, tab) => {
     if (tab.status === "complete" && pattern.test(tab.url)) {
         dataLoaded[tab.id] = loadData(tab.url);
-        chrome.pageAction.show(tab.id);
+        browser.pageAction.show(tab.id);
     }
 });
 
-chrome.tabs.onRemoved.addListener((tabId, info) => {
+browser.tabs.onRemoved.addListener(tabId => {
     delete dataLoaded[tabId];
 });
 
-chrome.storage.onChanged.addListener((changes, area) => {
-    chrome.tabs.query({active: true}, ([tab]) => {
+browser.storage.onChanged.addListener(async changes => {
+    if ("apiToken" in changes) {
+        let [tab] = await browser.tabs.query({ active: true });
         dataLoaded[tab.id] = loadData(tab.url);
-    });
+    }
 });
