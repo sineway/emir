8627ecd398a112d4849f4069085306a5682fe618
Sineway <sineway@yandex.com>
2021-07-06 17:13:31 +0300

    Add market gateway #getUser() and remove resolved values handling

--- api/MarketGateway.js
+++ api/MarketGateway.js
@@ -1,17 +1,17 @@
 /**
     @class MarketGateway
-    @param {Object=} properties
+    @param {Object=} fields
 */
 export class MarketGateway {
-    constructor(properties = {}) {
+    constructor(fields = {}) {
         /**
             @type {String}
         */
-        this.baseUrl = properties.baseUrl ?? "https://api.envato.com";
+        this.baseUrl = fields.baseUrl ?? "https://api.envato.com"
         /**
             @type {String}
         */
-        this.apiToken = properties.apiToken;
+        this.apiToken = fields.apiToken
     }
     /**
         @param {String} url
@@ -20,35 +20,40 @@ export class MarketGateway {
     async get(url) {
         let response = await fetch(this.baseUrl + url, {
             headers: { authorization: "bearer " + this.apiToken }
-        });
-        let json = await response.json();
+        })
+        let json = await response.json()
         if (json.error) {
-            throw new Error(json.description || json.error);
+            throw new Error(json.description ?? json.error)
         }
-        return json;
+        return json
     }
     /**
         @param {String} siteName
         @returns {Promise<Object>} https://build.envato.com/api#market_getPopular
     */
     async getPopular(siteName) {
-        let data = await this.get(`/v1/market/popular:${ siteName }.json`);
-        return data.popular;
+        return this.get(`/v1/market/popular:${ siteName }.json`)
     }
     /**
         @param {String} userName
-        @returns {Promise<Array>} https://build.envato.com/api#market_getUserBadges
+        @returns {Promise<Object>} https://build.envato.com/api#market_getUser
+    */
+    async getUser(userName) {
+        return this.get(`/v1/market/user:${ userName }.json`)
+    }
+    /**
+        @param {String} userName
+        @returns {Promise<Object>} https://build.envato.com/api#market_getUserBadges
     */
     async getUserBadges(userName) {
-        let data = await this.get(`/v1/market/user-badges:${ userName }.json`);
-        return data["user-badges"];
+        return this.get(`/v1/market/user-badges:${ userName }.json`)
     }
     /**
         @param {Number} itemId
         @returns {Promise<Object>} https://build.envato.com/api/#market_0_getCatalogItem
     */
     async getCatalogItem(itemId) {
-        return this.get(`/v3/market/catalog/item?id=${ itemId }`);
+        return this.get(`/v3/market/catalog/item?id=${ itemId }`)
     }
 }
-export default MarketGateway;
+export default MarketGateway
--- api/test/MarketGateway.test.js
+++ api/test/MarketGateway.test.js
@@ -1,49 +1,65 @@
-import MarketGateway from "/api/MarketGateway.js";
+import MarketGateway from "/api/MarketGateway.js"
 
 describe("MarketGateway", () => {
 
-    if (!/^[A-Za-z0-9]{32}$/.test(localStorage.apiToken)) {
-        localStorage.apiToken = prompt("API token");
+    let pattern = /^\w{32}$/
+    if (!pattern.test(localStorage.apiToken)) {
+        localStorage.apiToken = prompt("API token")
     }
     let gateway = new MarketGateway({
         apiToken: localStorage.apiToken
-    });
+    })
 
     describe("#getPopular()", () => {
 
-        it("resolves to a popular items/authors", async () => {
+        it("resolves to a popular files for a particular site", async () => {
             expect(
                 await gateway.getPopular("themeforest")
-            ).toEqual({
-                items_last_week: jasmine.any(Array),
-                items_last_three_months: jasmine.any(Array),
-                authors_last_month: jasmine.any(Array)
-            });
-        });
-    });
+            ).toEqual(
+                jasmine.objectContaining({
+                    popular: jasmine.any(Object)
+                })
+            )
+        })
+    })
+
+    describe("#getUser()", () => {
+
+        it("resolves to a user account details", async () => {
+            expect(
+                await gateway.getUser("ThemeFusion")
+            ).toEqual(
+                jasmine.objectContaining({
+                    user: jasmine.any(Object)
+                })
+            )
+        })
+    })
 
     describe("#getUserBadges()", () => {
 
-        it("resolves to a list of user badges", async () => {
+        it("resolves to a list of badges for a given user", async () => {
             expect(
                 await gateway.getUserBadges("ThemeFusion")
             ).toEqual(
-                jasmine.any(Array)
-            );
-        });
-    });
+                jasmine.objectContaining({
+                   "user-badges": jasmine.any(Array)
+                })
+            )
+        })
+    })
 
     describe("#getCatalogItem()", () => {
 
-        it("resolves to a catalog item", async () => {
-            let itemId = 2833226;
+        it("resolves to all details of a particular item on Envato Market", async () => {
+            let itemId = 2833226
             expect(
                 await gateway.getCatalogItem(itemId)
             ).toEqual(
                 jasmine.objectContaining({
                     id: itemId
                 })
-            );
-        });
-    });
-});
+            )
+        })
+    })
+})
