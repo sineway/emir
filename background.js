5efe71f77ac39242909613c878b40a905ba21bf7
Sineway <sineway@yandex.com>
2021-09-06 11:02:39 +0300

    Update background script

--- background.js
+++ background.js
@@ -1,38 +1,38 @@
-browser.contextMenus.removeAll().then(() => {
-    let patterns = [
-        "*://themeforest.net/item/*",
-        "*://codecanyon.net/item/*",
-        "*://videohive.net/item/*",
-        "*://audiojungle.net/item/*",
-        "*://graphicriver.net/item/*",
-        "*://photodune.net/item/*",
-        "*://3docean.net/item/*"
-    ];
-    let title = browser.i18n.getMessage("command");
+import PageOptions from "/ui/PageOptions.js"
 
-    browser.contextMenus.create({
-        contexts: ["page"],
-        id: "audit_document",
-        documentUrlPatterns: patterns,
-        title
-    });
-    browser.contextMenus.create({
-        contexts: ["link"],
-        id: "audit_target",
-        targetUrlPatterns: patterns,
-        title
-    });
-});
+browser.runtime.onInstalled.addListener(() => {
+	let patterns = [
+		"*://themeforest.net/item/*",
+		"*://codecanyon.net/item/*",
+		"*://videohive.net/item/*",
+		"*://audiojungle.net/item/*",
+		"*://graphicriver.net/item/*",
+		"*://photodune.net/item/*",
+		"*://3docean.net/item/*"
+	]
+	let title = browser.i18n.getMessage("contextMenu")
 
-browser.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId.startsWith("audit")) {
-        let url = info.linkUrl ?? info.pageUrl;
-        let [all, itemId] = url.match("/([0-9]+)");
-        browser.windows.create({
-            type: "popup",
-            url: `/audit.html?id=${ itemId }`,
-            width: 384,
-            height: 608
-        });
-    }
-});
+	browser.contextMenus.create({
+		contexts: ["page"],
+		id: "page",
+		documentUrlPatterns: patterns,
+		title
+	})
+	browser.contextMenus.create({
+		contexts: ["link"],
+		id: "link",
+		targetUrlPatterns: patterns,
+		title
+	})
+})
+
+browser.contextMenus.onClicked.addListener(async info => {
+	let [path, id] = (info.linkUrl ?? info.pageUrl).match(/\/item\/[\w-]+\/(\d+)/)
+	let settings = await browser.storage.local.get(new PageOptions)
+	browser.windows.create({
+		type: "popup",
+		url: `/estimates.html?${ new URLSearchParams({ id }) }`,
+		width: Math.round(settings.baseWidth * devicePixelRatio),
+		height: Math.round(settings.baseHeight * devicePixelRatio)
+	})
+})
