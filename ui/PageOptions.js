2f1e4a1bcc8ba394f261f5129a54b1f4eaaea3eb
Sineway <sineway@yandex.com>
2021-09-27 11:33:36 +0300

    Add page options

---
+++ ui/PageOptions.js
@@ -0,0 +1,22 @@
+/**
+	@class PageOptions
+*/
+export class PageOptions {
+	/**
+		@type {String}
+	*/
+	colorScheme = "light"
+	/**
+		@type {Number}
+	*/
+	hue = 45
+	/**
+		@type {Number}
+	*/
+	baseWidth = 384
+	/**
+		@type {Number}
+	*/
+	baseHeight = 608
+}
+export default PageOptions
