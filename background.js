1486db3ece50c53d250836461ea34e0e03a27c3f
Sineway <sineway@yandex.com>
2021-10-05 14:21:15 +0300

    Context menu: Remove page context

--- background.js
+++ background.js
@@ -1,31 +1,22 @@
 browser.runtime.onInstalled.addListener(() => {
-	let patterns = [
-		"*://themeforest.net/item/*",
-		"*://codecanyon.net/item/*",
-		"*://videohive.net/item/*",
-		"*://audiojungle.net/item/*",
-		"*://graphicriver.net/item/*",
-		"*://photodune.net/item/*",
-		"*://3docean.net/item/*"
-	]
-	let title = browser.i18n.getMessage("context_menu")
-
 	browser.contextMenus.create({
-		contexts: ["page"],
-		id: "page",
-		documentUrlPatterns: patterns,
-		title
-	})
-	browser.contextMenus.create({
-		contexts: ["link"],
+		title: browser.i18n.getMessage("context_menu"),
 		id: "link",
-		targetUrlPatterns: patterns,
-		title
+		contexts: ["link"],
+		targetUrlPatterns: [
+			"*://themeforest.net/item/*",
+			"*://codecanyon.net/item/*",
+			"*://videohive.net/item/*",
+			"*://audiojungle.net/item/*",
+			"*://graphicriver.net/item/*",
+			"*://photodune.net/item/*",
+			"*://3docean.net/item/*"
+		]
 	})
 })
 
 browser.contextMenus.onClicked.addListener(async info => {
-	let [path, id] = (info.linkUrl ?? info.pageUrl).match(/\/item\/[\w-]+\/(\d+)/)
+	let [path, id] = info.linkUrl.match(/\/item(?:\/[\w-]+)+\/(\d+)/)
 	let settings = await browser.storage.local.get()
 	browser.windows.create({
 		type: "popup",
