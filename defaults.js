bf56639a55bfeff53e4760026e5ff281b3920541
Sineway <sineway@ya.ru>
2024-04-30 09:59:04 +0300

    refactor: remove defaults.js

--- defaults.js
+++
@@ -1,9 +0,0 @@
-export default {
-    baseWidth: 384,
-    baseHeight: 612,
-    colorScheme: 'light',
-    hue: 45,
-    countryBadge: true,
-    authorLevelBadge: false,
-    usBuyersPercent: 40,
-};
